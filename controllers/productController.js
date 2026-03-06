b1c2dab03aa1485ab570dcebddbeb39b0af6988a
alas <janreytanjutco15@gmail.com>
2026-03-06 13:56:13 +0800

    Refactor productController to manage users instead of products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,26 +1,23 @@
 //SQL
-const connection = require("../config/db");
-//get all products
-exports.getAllGadgets = (req, res) => {
-  connection.query("SELECT * FROM items", (err, rows, fields) => {
-    if (err) throw err;
-    res.json(rows);
-  });
+const connection = require('../config/db');
+
+//Get all users
+exports.getAllUsers=(req,res)=>{
+    connection.query('SELECT * FROM userdata', (err,rows,fields)=>{
+        if(err) throw err;
+            res.json(rows);
+    });
 };
 
-exports.createProducts = (req, res) => {
-  const {itemName, unitPrice, quantity, supplier } = req.body;
-  connection.query(
-    "INSERT INTO items (itemName, unitPrice, quantity, supplier) VALUES (?, ?, ?, ?)",
-    [itemName, unitPrice, quantity, supplier],
-    (err, result) => {
-      if (err) throw err;
-      res.json({
-        message: "User created successfully",
-        id: result.insertId,
-      });
-    },
-  );
+
+//Create a new user
+//CRUD - Create
+exports.createUser=(req,res)=>{
+    const {productName,category,stockCount,locationCode,lastUpdated}=req.body;
+    connection.query('INSERT INTO userdata (product_name, category, stock_count, location_code, last_updated) VALUES (?,?,?,?,?)',[productName,category,stockCount,locationCode,lastUpdated],(err,result)=>{
+        if(err) throw err;
+            res.json({message: 'User created succesfully', userId:result.insertId});
+    });
 };
 
 
